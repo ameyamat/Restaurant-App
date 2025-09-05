964f6a98b3082a5337b033fc869c82f2a9d13c01
Amaan Ali Khan <amaanali.amaanali@gmail.com>
2025-09-06 00:35:11 +0530

    check

---
+++ src/main/java/RestaurantApp/Restaurant/controller/checkController.java
@@ -0,0 +1,13 @@
+package RestaurantApp.Restaurant.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class checkController {
+
+    @GetMapping("/hello")
+    public String getCheck(){
+        return "ok";
+    }
+}
